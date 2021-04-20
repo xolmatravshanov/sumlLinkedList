22fd64517f89323cd4ffa525a9248ce5a0fecd51
xolmat <xolmatrw@gmail.com>
2021-04-20 17:37:59 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Node class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+def addTwoLinkedList(ls1: LinkedList, ls2: LinkedList):
+
+    if ls1 == None or ls2 == None:
+        return LinkedList
+
+
+
+
+
